6c6fed1ab29346f4f39563cc7ea9cdb8e1a6fbee
= <=>
2021-01-24 16:45:26 -0600

    Pruebaas unitarias

---
+++ GloboTicket.TicketManagement.API.IntegrationTests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace GloboTicket.TicketManagement.API.IntegrationTests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
